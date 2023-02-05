82650ecb64d5183dc2f5436b079f7291edade293
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-05 21:30:57 +0300

    check requirements

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Determine if all boxes can be opened"""
+""""You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
+
+Write a method that determines if all the boxes can be opened."""
 
 
 def canUnlockAll(boxes):

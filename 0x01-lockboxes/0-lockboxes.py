238dd169440b4d321033813b0f430ecbbd16231e
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-05 21:22:49 +0300

    unlock all boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,4 +3,14 @@
 
 
 def canUnlockAll(boxes):
-    return True
+    """Determine if all boxes can be opened"""
+    x = len(boxes)
+    aList = [0]
+    for i in my list:
+        for j in boxes[i]:
+            if j not in aList:
+                if j < x:
+                    aList.append(j)
+    if len(aList) == x:
+        return True
+    return False
